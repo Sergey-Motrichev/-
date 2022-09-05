ebfcc8cdde68ac586388c72bb64b114b55fa0f88
Sergey-Motrichev <112479065+Sergey-Motrichev@users.noreply.github.com>
2022-09-05 12:27:12 +0200

    решение

---
+++ задача01/Program.cs
@@ -0,0 +1,13 @@
+﻿int a=-9;
+int b=-3;
+
+if(a>b){
+    System.Console.WriteLine("Число a больше числа b");
+}
+else     
+    if(b>a){
+      System.Console.WriteLine("Число b больше числа a");   
+    }
+else{
+        System.Console.WriteLine("Числа равны");
+};
